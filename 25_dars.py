3bb51ea897875d482bc37dfdb217dec969c5e04c
Hoja1 <78483605+Hoja1@users.noreply.github.com>
2021-07-06 11:34:58 +0500

    Create 25_dars.py

---
+++ 25_dars.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 11:15:16 2021
+
+@author: user
+"""
+
+
+################################################ if else aperatiri
+
+# cars = ['audi','bmw', 'malibu']
+# for car in cars:
+#  if car == 'bmw':
+#       print(car.upper())
+# else:
+#       print(car.title())
+ 
+yil = int(input("Tug'ilgan yilingizni kriting ?\n>>>"))
+if 2021-yil<18:
+   print(f"yashingiz:{2021-yil}da ekan.")
+   print("krish mumkin emas")
+else:
+   print("xush kelibsiz(-_-)")
+
+# yosh = int(input("yoshingzni kriting \n>>>"))
+# if yosh>=18:
+#    print("salom")
+# else:
+#    print("yoshingiz yetmas ekan")
